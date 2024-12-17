ee5099bbca2d92d8a7ac57ea925dc3e492f72ecb
Enes Ceylan <contact@enesceylan.dev>
2024-12-17 17:56:37 +0300

    Changed to hello world.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using Raylib_cs;
-
-namespace HelloWorld;
+﻿using System;
 
 class Program
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        Raylib.InitWindow(800, 480, "Hello World");
-
-        while (!Raylib.WindowShouldClose())
-        {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);
-
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
-
-            Raylib.EndDrawing();
-        }
-
-        Raylib.CloseWindow();
+        Console.WriteLine("Hello, World!");
     }
 }
